f3ad9b068adaf1aa31fcf103d0605c39312c020b
Himanshu Dixit <hudixt@gmail.com>
2017-06-17 15:33:41 +0530

    Load and delete function

--- src/SocialPostManager.php
+++ src/SocialPostManager.php
@@ -7,13 +7,10 @@ use Drupal\Core\Entity\EntityTypeManagerInterface;
 use Drupal\Core\Entity\Query\QueryFactory;
 use Drupal\Core\Session\AccountProxy;
 
-
 /**
  * Contains all logic that is related to Drupal user management.
  */
-
-class SocialPostManager
-{
+class SocialPostManager {
 
   protected $loggerFactory;
   protected $entityQuery;
@@ -46,9 +43,7 @@ class SocialPostManager
    * @param \Drupal\Core\Session\AccountProxy $current_user
    *   Used to get current active user.
    */
-
-  public function __construct(LoggerChannelFactoryInterface $logger_factory, EntityTypeManagerInterface $entity_type_manager, QueryFactory $entityQuery, AccountProxy $current_user)
-  {
+  public function __construct(LoggerChannelFactoryInterface $logger_factory, EntityTypeManagerInterface $entity_type_manager, QueryFactory $entityQuery, AccountProxy $current_user) {
     $this->loggerFactory = $logger_factory;
     $this->entityTypeManager = $entity_type_manager;
     $this->entityQuery = $entityQuery;
@@ -56,6 +51,7 @@ class SocialPostManager
     // Sets default plugin id.
     $this->setPluginId('social_post');
   }
+
   /**
    * Sets the implementer plugin id.
    *
@@ -97,10 +93,10 @@ class SocialPostManager
    *   False otherwise
    */
   public function addRecord($pluginId, $provider_user_id, $token) {
-    //Get User ID of logged in user
+    // Get User ID of logged in user.
     $user_id = $this->currentUser->id();
 
-    //Encode token into json format.
+    // Encode token into json format.
     $json_token = json_encode($token);
 
     // Generate an initialization vector to be used for encyrption.
@@ -110,11 +106,11 @@ class SocialPostManager
       'user_id' => $user_id,
       'plugin_id' => $pluginId,
       'provider_user_id' => $provider_user_id,
-      'token' => $this->encryptToken($json_token)
+      'token' => $this->encryptToken($json_token),
     ];
 
     $user_info = $this->entityTypeManager()->getStorage('social_post')->create($values);
-    if($user_info) {
+    if ($user_info) {
       return TRUE;
     }
     else {
@@ -122,11 +118,46 @@ class SocialPostManager
     }
 
   }
+
+  /**
+   * Update token of a praticular record.
+   *
+   * @param string $pluginId
+   *   Type of social network.
+   * @param string $provider_user_id
+   *   Unique Social ID returned by social network.
+   * @param string $token
+   *   Token provided by social_network.
+   *
+   * @return True if updated.
+   */
   public function updateToken($pluginId, $provider_user_id, $token) {
+    $field_storage_configs = $this->entityTypeManager
+      ->getStorage('social_auth')
+      ->loadByProperties(array('plugin_id' => $pluginId, 'provider_user_id' => $provider_user_id));
 
+    foreach ($field_storage_configs as $field_storage) {
+      $field_storage->token = $token;
+      $field_storage->enforceIsNew(FALSE);
+      $field_storage->save();
+    }
   }
-  public function deleteRecord($pluginId, $provider_user_id) {
 
+  /**
+   * Delete record from entity table.
+   *
+   * @param string $pluginId
+   *   Type of social network.
+   * @param string $provider_user_id
+   *   Unique Social ID returned by social network.
+   *
+   * @return True if deleted.
+   */
+  public function deleteRecord($pluginId, $provider_user_id) {
+    $this->entityTypeManager
+      ->getStorage('social_auth')
+      ->loadByProperties(array('plugin_id' => $pluginId, 'provider_user_id' => $provider_user_id))
+      ->delete();
   }
 
   /**
@@ -138,7 +169,6 @@ class SocialPostManager
    *   Unique Social ID returned by social network.
    *
    * @return Token in array format
-   *   else return False.
    */
   public function getToken($pluginId, $provider_user_id) {
 
@@ -161,33 +191,67 @@ class SocialPostManager
       $decrypted_token = $this->decryptToken($user_data->get('token')->getValue()[0]['value']);
 
       // Convert token format from json to array.
-      return $json_decode($decrypted_token);
+      return json_decode($decrypted_token);
     }
 
   }
 
+  /**
+   * Encrypt the token.
+   *
+   * @param string $token
+   *   Tokens provided by social provider.
+   *
+   * @return Encrypted token in array format
+   */
   private function encryptToken($token) {
     $key = $this->key;
 
-    // Remove the base64 encoding from our key
+    // Remove the base64 encoding from our key.
     $encryption_key = base64_decode($key);
-    // Generate an initialization vector
+    // Generate an initialization vector.
     $iv = openssl_random_pseudo_bytes(openssl_cipher_iv_length('aes-256-cbc'));
-    // Encrypt the data using AES 256 encryption in CBC mode using our encryption key and initialization vector.
+    // Encrypt the data using AES 256 encryption in CBC mode
+    // using our encryption key and initialization vector.
     $encrypted = openssl_encrypt($token, 'aes-256-cbc', $encryption_key, 0, $iv);
-    // The $iv is just as important as the key for decrypting, so save it with our encrypted data using a unique separator (::)
+    // The $iv is just as important as the key for decrypting,
+    // so save it with our encrypted data using a unique separator (::).
     return base64_encode($encrypted . '::' . $iv);
   }
 
+  /**
+   * Decrypt the encrypted token.
+   *
+   * @param string $token
+   *   Encrypted token stored in database.
+   *
+   * @return decrypted token.
+   */
   private function decryptToken($token) {
     $key = $this->key;
-    // Remove the base64 encoding from our key
+
+    // Remove the base64 encoding from our key.
     $encryption_key = base64_decode($key);
 
-    // To decrypt, split the encrypted data from our IV - our unique separator used was "::"
+    // To decrypt, split the encrypted data from our IV -
+    // our unique separator used was "::".
     list($encrypted_data, $iv) = explode('::', base64_decode($token), 2);
     return openssl_decrypt($encrypted_data, 'aes-256-cbc', $encryption_key, 0, $iv);
 
+  }
+
+  /**
+   * Get salt for this drupal installation.
+   *
+   * @return unique identifier.
+   */
+  function getSalt() {
+    $hash_salt = self::$instance->get('hash_salt');
+
+    if (empty($hash_salt)) {
+      return FALSE;
+    }
+    return $hash_salt;
   }
 
 }
